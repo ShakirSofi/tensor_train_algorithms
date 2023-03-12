59b18596165e8c1669a8ed58301b6349e9a4c6ce
ShakirSofi <82702981+ShakirSofi@users.noreply.github.com>
2023-03-12 21:26:32 +0100

    Add files via upload

--- mlsvd_miss_shakir/give_miss_3.m
+++ mlsvd_miss_shakir/give_miss_3.m
@@ -2,7 +2,7 @@ function [Ym, r,c] = give_miss_3(Ym, p)
 if nargin < 2 || isempty(p)
     p = 0.1;
 end
-Index    = randperm(numel(Ym(:,:,1)), ceil(size(Ym,3)*p)); 
+Index    = randperm(numel(Ym(:,:,1)), ceil(numel(Ym(:,:,1))*p)); 
 [r, c] = ind2sub(size(Ym(:,:,1)), Index);
 
   for i=1:length(r)
