6337c0556a0ed522f69ac60e0c41bd66d1d19363
ShakirSofi <82702981+ShakirSofi@users.noreply.github.com>
2025-07-23 16:29:13 +0200

    Add files via upload

---
+++ TT_full.m
@@ -0,0 +1,38 @@
+function [x_full] = TT_full(cores, ranks, sze)
+%% converts TT cores in cell to full tensor
+d=length(cores);
+if ndims(cores{1}) == 2 && size(cores{1}, 1) ~= 1
+    sz = size(cores{1});
+    cores{1} = reshape(cores{1}, [1, sz]);
+end  
+
+if nargin < 3 || isempty(sze)
+    % Size of tensor
+    sze = [];
+    for i=1:d
+         sze=[sze size(cores{i},2)];
+    end
+end
+if nargin < 2 || isempty(ranks)
+   % TT_ranks % ranks =[1 r1 r2 r3.. 1]
+   ranks = [];
+   ranks(1) = 1;
+   for i=1:d-1
+       ranks=[ranks size(cores{i},3)];
+   end
+   ranks(end+1)=1;
+end
+
+%%
+x_full = cores{1};
+for k=2:d
+   x_full = reshape(x_full, [numel(x_full)/ranks(k), ranks(k)]);
+   %size(x_full)
+   cr = cores{k};
+   %size(cr)
+   cr = reshape(cr, [ranks(k), numel(cr)/ranks(k)]);
+   x_full = x_full*cr;
+end
+x_full=reshape(x_full, sze);
+
+end
